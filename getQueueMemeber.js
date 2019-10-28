5ef27f9f4df2f4130aadf6709933602cc795d3cb
Francis San Filippo <fsanfilipp@vailsys.com>
2019-10-28 11:03:16 -0500

    freeclimb code changes

--- getQueueMemeber.js
+++ getQueueMemeber.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
-const persephonySDK = require('@persephony/sdk')
-// your Persephony API key (available in the Dashboard) - be sure to set up environment variables to store these values
+const freeclimbSDK = require('@freeclimb/sdk')
+// your freeclimb API key (available in the Dashboard) - be sure to set up environment variables to store these values
 const accountId = process.env.ACCOUNT_ID
 const authToken = process.env.AUTH_TOKEN
-const persephony = persephonySDK(accountId, authToken)
+const freeclimb = freeclimbSDK(accountId, authToken)
 
 // Invoke get Queue Member call
-persephony.api.queues.members(queueId).get(callId).then(member => {
+freeclimb.api.queues.members(queueId).get(callId).then(member => {
   // Use the queue member
 }).catch(err => {/** Catch Errors */ })
